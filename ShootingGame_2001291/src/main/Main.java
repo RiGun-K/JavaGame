61ac3f9c2fe3f475bb9bf5a5dc0d2d0bb7f90482
RiGun-K <1234suho@naver.com>
2021-11-15 19:23:20 +0900

    11/15

---
+++ ShootingGame_2001291/src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Main {
+
+	// 게임 해상도 설정 
+    public static final int SCREEN_WIDTH = 1080;
+    public static final int SCREEN_HEIGHT = 620;
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new ShootingGame();
+	}
+
+}
