863e1744b5b54bbf634e156960c3d43b7e9f1a03
RiGun-K <1234suho@naver.com>
2021-11-19 12:28:55 +0900

    11/19

--- ShootingGame_2001291/src/database/User.java
+++ ShootingGame_2001291/src/database/User.java
@@ -2,6 +2,35 @@ package database;
 
 public class User {
 
+	private int id;
+	private int pw;
+	
+	User(int id, int pw) {
+		this.id = id;
+		this.pw = pw;
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public int getPw() {
+		return pw;
+	}
+
+
+	public void setPw(int pw) {
+		this.pw = pw;
+	}
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
